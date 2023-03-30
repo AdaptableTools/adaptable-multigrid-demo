2fe5876ca7ed581a8e8165c6839ac78bb667a7b6
Andrei Floricel <andrei.floricel@gmail.com>
2023-03-30 11:27:24 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { AdaptableAgGrid } from './AdaptableAgGrid';
+
+function App() {
+  return (
+    <div className="text-center selection:bg-green-900">
+      <AdaptableAgGrid></AdaptableAgGrid>
+    </div>
+  );
+}
+
+export default App;
